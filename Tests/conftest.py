9af189aeb11d1f7cb2f26080f645a017f321dbce
pikuqa <pikupal.2012@gmail.com>
2024-11-13 17:30:59 +0530

    requirement2.txt

--- Tests/conftest.py
+++ Tests/conftest.py
@@ -1,5 +1,6 @@
 import pytest
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 from Config.config import Testdata
 
@@ -7,9 +8,10 @@ from Config.config import Testdata
 
 @pytest.fixture(params=["chrome"], scope="class")
 def init_driver(request):
-    s3=Service(Testdata.CHROME_executable_path)
+    #s3=Service(Testdata.CHROME_executable_path)
     if request.param=="chrome":
-        driver=webdriver.Chrome(service=s3)
+        #driver=webdriver.Chrome(service=s3)
+        driver = webdriver.Chrome(ChromeDriverManager().install())
     request.cls.driver=driver
     driver.maximize_window()
     driver.implicitly_wait(10)
